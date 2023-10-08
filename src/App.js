aceb99b84dfaf26a82e7f487d5b2cc66e1c9bd18
Juan Villegas <juancarlos.villegas@gmail.com>
2023-10-08 17:23:54 -0600

    Begin with navigation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Container, Nav } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Navbar>
+          <Container>
+              <Navbar.Brand>My Personal Library</Navbar.Brand>
+              <Nav>
+                  <Nav.Item>Created by Juan</Nav.Item>
+              </Nav>
+          </Container>
+      </Navbar>
   );
 }
 
