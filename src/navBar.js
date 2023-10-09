a56e254a89288c7352fa24b74d015ca65f1e5a76
Juan Villegas <juancarlos.villegas@gmail.com>
2023-10-09 17:29:31 -0600

    Create nav bar

---
+++ src/navBar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Link } from "react-router-dom";
+function NavBar() {
+    return (
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand>My Read Books</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mx-auto">
+                        <Link to="/create" className="nav-link">
+                            Create
+                        </Link>
+                        <Nav.Link href="#read">Read</Nav.Link>
+                        <Nav.Link href="#update">Update</Nav.Link>
+                        <Nav.Link href="#delete">Delete</Nav.Link>
+                    </Nav>
+                    <Nav className="ml-auto">
+                        <Nav.Item>Created by Juan</Nav.Item>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default NavBar;
