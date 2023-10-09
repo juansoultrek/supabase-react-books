0ac80212736837ed8571eccccb08ddc0ebefaff8
Juan Villegas <juancarlos.villegas@gmail.com>
2023-10-09 17:30:05 -0600

    Create main ROuter

--- src/App.js
+++ src/App.js
@@ -1,50 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navbar, Container, Nav, Form, Row, Col, Button } from 'react-bootstrap';
-import {useState} from "react";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from './NavBar'; // Make sure to import your NavBar component
+import CreateBook from './CreateBook'; // Import your CreateBook component
 
 function App() {
-    const [title, setTitle] = useState("");
-    const [author, setAuthor] = useState("");
-
-    console.log(title);
-    console.log(author);
-
-
     return (
-        <>
-            <Navbar>
-                <Container>
-                    <Navbar.Brand>My Read Books </Navbar.Brand>
-                    <Nav>
-                        <Nav.Item>Created by Juan</Nav.Item>
-                    </Nav>
-                </Container>
-            </Navbar>
-            <Container>
-                <Row>
-                    <Col xs={12} md={8}>
-                        <h3>Add Book For Supabase Database</h3>
-                        <Form.Label>Book Name</Form.Label>
-                        <Form.Control
-                            type = "text"
-                            id="title"
-                            onChange={(e) => setTitle(e.target.value)}
-                        />
-                        <h3>Add Book For Supabase Database</h3>
-                        <Form.Label>Book Description</Form.Label>
-                        <Form.Control
-                            type = "text"
-                            id="author"
-                            onChange={(e) => setAuthor(e.target.value)}
-                        />
-                        <br></br>
-                            <Button>Add Book</Button>
-                    </Col>
-                </Row>
-            </Container>
-        </>
+        <Router>
+            <div>
+                <NavBar />
+                <Routes>
+                    <Route path="/create" element={<CreateBook />} />
+                    {/* Add more routes as needed */}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
