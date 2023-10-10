eda2438c5764d69ecaacba2e6635cb7870903b00
Juan Villegas <juancarlos.villegas@gmail.com>
2023-10-10 17:44:29 -0600

    Only have home with read bookx and add new book in menu

--- src/navBar.js
+++ src/navBar.js
@@ -2,20 +2,18 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import { Link } from "react-router-dom";
+
 function NavBar() {
     return (
         <Navbar bg="light" expand="lg">
             <Container>
-                <Navbar.Brand>My Read Books</Navbar.Brand>
+                <Link to="/read" className="navbar-brand">My Read Books</Link> {/* Added Link to Read Books */}
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mx-auto">
                         <Link to="/create" className="nav-link">
-                            Create
+                            Add New Book
                         </Link>
-                        <Nav.Link href="#read">Read</Nav.Link>
-                        <Nav.Link href="#update">Update</Nav.Link>
-                        <Nav.Link href="#delete">Delete</Nav.Link>
                     </Nav>
                     <Nav className="ml-auto">
                         <Nav.Item>Created by Juan</Nav.Item>
