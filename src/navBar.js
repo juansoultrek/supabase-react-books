7e205df902f4c97b65994de900dbc8fa064f22d4
Juan Villegas <juancarlos.villegas@gmail.com>
2023-10-11 15:58:09 -0600

    Add menu option to create, edit and login

--- src/navBar.js
+++ src/navBar.js
@@ -2,18 +2,29 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import { Link } from "react-router-dom";
-
 function NavBar() {
     return (
         <Navbar bg="light" expand="lg">
             <Container>
-                <Link to="/read" className="navbar-brand">My Read Books</Link> {/* Added Link to Read Books */}
+                <Navbar.Brand>My Read Books</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mx-auto">
-                        <Link to="/create" className="nav-link">
-                            Add New Book
+                        <Link to="/login" className="nav-link">
+                            Login
                         </Link>
+                        <Link to="/create" className="nav-link">
+                            Add  Book
+                        </Link>
+                        <Link to="/read" className="nav-link">
+                        </Link>
+
+                        <Link to="/edit" className="nav-link">
+                            Edit Book
+                        </Link>
+                        <Link to="/read" className="nav-link">
+                        </Link>
+
                     </Nav>
                     <Nav className="ml-auto">
                         <Nav.Item>Created by Juan</Nav.Item>
