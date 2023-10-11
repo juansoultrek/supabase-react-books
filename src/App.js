07b452d9375ab62c14e44780ab9594f75dc58e3c
Juan Villegas <juancarlos.villegas@gmail.com>
2023-10-11 15:56:26 -0600

    Redirect to read page if user has not logged in.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import NavBar from './navBar'; // Make sure to import your NavBar component
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import NavBar from './navBar';
 import CreateBook from './createBook';
-import ReadBook from "./readBook";
+import ReadBook from './readBook';
+import ProtectedRoute from './protectedRoute';  // Assuming you have a ProtectedRoute component
+
 function App() {
     return (
         <Router>
             <div>
                 <NavBar />
                 <Routes>
-                    <Route path="/create" element={<CreateBook />} />
+                    <Route path="/create" element={<ProtectedRoute element={<CreateBook />} />} />
                     <Route path="/read" element={<ReadBook />} />
-                    <Route path="/edit" element={<ReadBook />} />
+                    <Route path="/edit" element={<ProtectedRoute element={<ReadBook />} />} />
                     <Route path="*" element={<Navigate to="/read" />} />
                 </Routes>
             </div>
