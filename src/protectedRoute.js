4f4a4e2130c4158896c52e413440b728c87c7332
Juan Villegas <juancarlos.villegas@gmail.com>
2023-10-11 17:31:08 -0600

    Fix protected route

--- src/protectedRoute.js
+++ src/protectedRoute.js
@@ -2,26 +2,26 @@ import React, { useState, useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
 import { supabase } from './supabaseClient';
 
-function ProtectedRoute(props) {
-    const [user, setUser] = useState(null);
+function ProtectedRoute({ element }) {
+    const [session, setSession] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
-        const checkUser = async () => {
-            // Get the session from supabase
-            const { data: session, error } = await supabase.auth.getSession();
+        console.log('Checking session...');
+        supabase.auth.getSession().then(({ data: { session } }) => {
+            console.log('Session data:', session);
+            setSession(session);
+        });
 
-            if (session && session.user) {
-                setUser(session.user);
-                console.log('User logged in:', session.user);
-            } else {
-                console.log('User not logged in');
-            }
-        };
-
-        checkUser();
+        supabase.auth.onAuthStateChange((_event, session) => {
+            console.log('Auth state change event:', _event);
+            console.log('Session state:', session);
+            setSession(session);
+        });
     }, []);
 
-    return user ? props.children : <Navigate to="/login" />;
+    return session ? element : <Navigate to="/login" />;
 }
 
 export default ProtectedRoute;
