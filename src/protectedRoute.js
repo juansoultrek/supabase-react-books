f108b215052c837ee36d08bc529cc969c79fdbd3
Juan Villegas <juancarlos.villegas@gmail.com>
2023-10-11 19:32:17 -0600

    Remove logs.

--- src/protectedRoute.js
+++ src/protectedRoute.js
@@ -8,16 +8,12 @@ function ProtectedRoute({ element }) {
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        console.log('Checking session...');
         supabase.auth.getSession().then(({ data: { session } }) => {
-            console.log('Session data:', session);
             setSession(session);
             setLoading(false);
         });
 
         supabase.auth.onAuthStateChange((_event, session) => {
-            console.log('Auth state change event:', _event);
-            console.log('Session state:', session);
             setSession(session);
         });
     }, []);
