f33dcbd0437a065af025041ca83f8e6e7054ac16
Juan Villegas <juancarlos.villegas@gmail.com>
2023-10-11 18:03:51 -0600

    Add a log out button

--- src/navBar.js
+++ src/navBar.js
@@ -2,7 +2,18 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import { Link } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
+import { supabase } from './supabaseClient';
+
 function NavBar() {
+    const navigate = useNavigate();
+
+    const handleLogout = async () => {
+
+        const { error } = await supabase.auth.signOut();
+        navigate("read");
+    }
+
     return (
         <Navbar bg="light" expand="lg">
             <Container>
@@ -14,17 +25,16 @@ function NavBar() {
                             Login
                         </Link>
                         <Link to="/create" className="nav-link">
-                            Add  Book
+                            Add Book
                         </Link>
                         <Link to="/read" className="nav-link">
                         </Link>
-
                         <Link to="/edit" className="nav-link">
                             Edit Book
                         </Link>
-                        <Link to="/read" className="nav-link">
+                        <Link to="#" className="nav-link" onClick={handleLogout}>
+                            Log Out
                         </Link>
-
                     </Nav>
                     <Nav className="ml-auto">
                         <Nav.Item>Created by Juan</Nav.Item>
