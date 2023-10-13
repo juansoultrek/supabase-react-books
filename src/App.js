9ffa53360fd3ab0f692b8cb77b26acf3c21ea618
Juan Villegas <juancarlos.villegas@gmail.com>
2023-10-12 18:10:15 -0600

    Add option to edit books in router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-d
 import NavBar from './navBar';
 import CreateBook from './createBook';
 import ReadBook from './readBook';
+import EditBook from './editBook';
 import LoginBook from './loginBook';
 import ProtectedRoute from './protectedRoute';  // Assuming you have a ProtectedRoute component
 
@@ -14,7 +15,7 @@ function App() {
                 <Routes>
                     <Route path="/create" element={<ProtectedRoute element={<CreateBook />} />} />
                     <Route path="/read" element={<ReadBook />} />
-                    <Route path="/edit" element={<ProtectedRoute element={<ReadBook />} />} />
+                    <Route path="/edit" element={<ProtectedRoute element={<EditBook />} />} />
                     <Route path="/login" element={<LoginBook />} />
                     <Route path="*" element={<Navigate to="/read" />} />
                 </Routes>
